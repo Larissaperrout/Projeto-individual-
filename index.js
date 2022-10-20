19bf5cf808563b15aba49b4e84e026e73b73ecd8
larissa <larissaperrout2021@gmail.com>
2022-10-20 16:55:49 -0300

    projetindividual

---
+++ index.js
@@ -0,0 +1,15 @@
+var values = new Array ("a","b","c","d","e","f","g","h","i","j","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z")
+var text = +document.getElementById("text").innerHTML
+
+function prev (){
+if(text >1){
+    text --;
+    document.getElementById("text").innerHTML= text;
+
+}
+}
+function calculete (){
+    
+}
+       
+    
