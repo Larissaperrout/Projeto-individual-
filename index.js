22200a552a75c6b31a2aa3d6cdd7b688b8873591
larissa <larissaperrout2021@gmail.com>
2022-10-24 16:59:45 -0300

    add trabalho

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-var values = new Array ("a","b","c","d","e","f","g","h","i","j","l","m","n","o","p","q","r","s","t","u","v","x","w","y","z")
-var text = +document.getElementById("text").innerHTML
+var texEntrada = document.getElementById ('texEntrada');
+var escolha = document.getElementById('opcoes');
+var numero= document.getElementById('numeros');
+var bcodificar= document.getElementById('codificar');
+var bdecodificar=document.getElementById('decodificar');
+var submit= document.querySelector('#submit');
+var mensagenSaida=document.querySelector('.mensagemSaida');
+var codificar1= document.querySelector('#codificar1');
+var decodificarr=document.getElementById('decodificarr');
+var radioInput=document.querySelector('.radioInput');
 
-function prev (){
-if(text >1){
-    text --;
-    document.getElementById("text").innerHTML= text;
 
-}
-}
-function calculete (){
+escolha.addEventListener('change', function() {
+
+    if (escolha.value == 'cifradeCesar') {
+       numero.style.display ='block'; 
+    }else if (escolha.value == 'opicao2') {
+        numero.style.display= 'none';
+    }else {
+        alert ('Escolha uma das opções!');
+    }
+})
+bcodificar.addEventListener("click", function(){
+    submit.style.display = 'block';
+    submit.innerText= 'Codificar'
+})
+bdecodificar.addEventListener("click", function(){
+    submit.style.display='block';
+    submit.innerText = 'Decodificar'
+})
+function codigoCesar(){
+
+    var menagemElemento= texEntrada.value;
+    var minusculaMensagem=menagemElemento.toloWeCase();
+    var numeroTransforme= (Number(numero.value)%26);
+    var mensagemCodificada='';
+
     
-}
-       
-    
+
+
+    }
+    //BASE 64//
+
+    function baSe64 (){
+
+        let mensagem= document.querySelector('#textEntrada').value
+        let decodificar= atob(mensagem)
+        return codificar1
+        
+    }
